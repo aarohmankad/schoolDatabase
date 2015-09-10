f8dce5fc5f2f85e60f9dfecdb343d457838910e1
unknown <149003115@mvhsrop901-19.vusdnoc.vusd.k12.ca.us>
2015-09-10 10:22:29 -0700

    initial commit

---
+++ src/school/Person.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package school;
+
+/**
+ *
+ * @author 149003115
+ */
+public class Person {
+    String name;
+    enum Gender {
+        MALE,
+        FEMALE,
+    };
+    Gender gender;
+    
+    Person(String _name, Gender _gender) {
+        name = name;
+        gender = gender;
+    };
+    
+    
+}
